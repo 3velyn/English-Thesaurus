fe3cc9ff254383bd7c35374cd939825a7aa72b1a
Evelina Spasova <spasova.evelina@gmail>
2021-10-25 16:35:49 +0300

    search for close match of the entry

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 
+from difflib import get_close_matches
+
 import json, io, sys
 
 sys.stdin.reconfigure(encoding='utf-8')
@@ -11,18 +13,17 @@ Builder.load_file('design.kv')
 
 class ThesaurusScreen(Screen):
 
-
     def get_description(self, phrase):
         dict = json.load(io.open('data.json', encoding='utf-8'))
-        phrase = phrase.lower()
+        best_match = get_close_matches(phrase.lower(), dict.keys())
 
-        if phrase in dict:
-            description = ''
-            for d in dict[phrase]:
+        if len(best_match) > 0 and best_match[0] in dict:
+            description = 'Description for "%s": \n' % best_match[0].title()
+            for d in dict[best_match[0]]:
                 description += d + '\n'
             self.ids.description.text = description
         else:
-            self.ids.description.text = 'Nemaaa'
+            self.ids.description.text = "This word or phrase does't exist. Please double check it."
 
 
 

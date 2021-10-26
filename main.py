a528e39a9e667eed5a64eb629e1006165518f68d
Evelina Spasova <spasova.evelina@gmail>
2021-10-26 16:08:04 +0300

    add popup for a typo in the word

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from kivy.core import text
 from kivy.lang import Builder
+from kivy.uix.behaviors import button
+from kivy.uix.button import Button, ButtonBehavior
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.popup import Popup
 from kivy.app import App
 
 from difflib import get_close_matches
 
 import json, io, sys
+
+from kivy.uix.widget import Widget
 
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
@@ -17,20 +26,38 @@ class ThesaurusScreen(Screen):
         dict = json.load(io.open('data.json', encoding='utf-8'))
         best_match = get_close_matches(phrase.lower(), dict.keys())
 
-        if len(best_match) > 0 and best_match[0] in dict:
-            description = 'Description for "%s": \n' % best_match[0].title()
-            for d in dict[best_match[0]]:
-                description += d + '\n'
+        if phrase.lower() in dict:
+            description = 'Description for "%s": \n' % phrase.title()
+            count = 1
+
+            for d in dict[phrase.lower()]:
+                description += '%s. %s\n' % (str(count), d)
+                count += 1
+
             self.ids.description.text = description
+
+        elif len(best_match) > 0:
+            Popups.show_popup(self, best_match[0])
+
         else:
             self.ids.description.text = "This word or phrase does't exist. Please double check it."
 
+class Popups(FloatLayout):
 
+    def show_popup(self, phrase):
+        layout = Popups()
+        layout.ids.popup_label.text = 'Did you mean "%s" instead?' % phrase.title()
+    
+        popup_window = Popup(title='Did you mean..?', 
+            content=layout, size_hint=(None, None), size=(400, 300))
+        popup_window.open()
 
+        layout.ids.yes_btn.bind(on_press=lambda x: ThesaurusScreen.get_description(self, phrase))
+        layout.ids.yes_btn.bind(on_press=popup_window.dismiss)
+        layout.ids.close_btn.bind(on_press=popup_window.dismiss)
+        
 
-
-class RootWidget(ScreenManager):
-    pass
+class RootWidget(ScreenManager): pass
 
 class MainApp(App):
     def build(self):

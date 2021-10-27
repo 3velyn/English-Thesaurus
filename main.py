30b930b9c876e70472f53d281a6f66f4016cc058
Evelina Spasova <spasova.evelina@gmail>
2021-10-27 13:13:45 +0300

    adding popup for typos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.lang import Builder
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.popup import Popup
@@ -16,40 +17,47 @@ Builder.load_file('design.kv')
 class ThesaurusScreen(Screen):
 
     def get_description(self, phrase):
-        dict = json.load(io.open('data.json', encoding='utf-8'))
-        best_match = get_close_matches(phrase.lower(), dict.keys())
-
-        if phrase.lower() in dict:
-            description = 'Description for "%s": \n' % phrase.title()
-            count = 1
-
-            for d in dict[phrase.lower()]:
+        dict = json.load(io.open('data.json'))
+        best_matches = get_close_matches(phrase, dict.keys())
+        description = ''
+        
+        count = 1
+        if best_matches[0] == phrase and best_matches[0] in dict:
+            description = 'Description for "%s": \n' % best_matches[0]
+            for d in dict[best_matches[0]]:
                 description += '%s. %s\n' % (str(count), d)
                 count += 1
 
-            self.ids.description.text = description
-
-        elif len(best_match) > 0:
-            Popups.show_popup(self, best_match[0])
+        elif len(best_matches) > 0:
+            self.show_popup(best_matches)
 
         else:
-            self.ids.description.text = "This word or phrase does't exist. Please double check it."
+            description = "This word or phrase does't exist. Please double check it."
 
-class Popups(FloatLayout):
+        self.ids.description.text = description
 
-    def show_popup(self, phrase):
-        layout = Popups()
-        layout.ids.popup_label.text = 'Did you mean "%s" instead?' % phrase.title()
+    def show_popup(self, best_matches):
+        box = Popups()
     
-        popup_window = Popup(title='Did you mean..?', 
-            content=layout, size_hint=(None, None), size=(400, 300))
-        popup_window.open()
+        self.popup_window = Popup(title='Did you mean..?', 
+            content=box, size_hint=(None, None), size=(400, 300))
+        self.popup_window.open()
 
-        layout.ids.yes_btn.bind(on_press=lambda x: ThesaurusScreen.get_description(self, phrase))
-        layout.ids.yes_btn.bind(on_press=popup_window.dismiss)
-        layout.ids.close_btn.bind(on_press=popup_window.dismiss)
+        y = 0.75
+        for match in best_matches:
+            self.add_button(match, y)
+            y -= 0.22
+
+        box.ids.close_btn.bind(on_press=self.popup_window.dismiss)
+
+    def add_button(self, match, y):
+        btn = Button(text=match, size_hint=(0.8, 0.2), pos_hint={'x': 0.1, 'y': y})
+        btn.bind(on_press=lambda x: self.get_description(match))
+        btn.bind(on_release=self.popup_window.dismiss)
+        self.popup_window.content.add_widget(btn)
+
+class Popups(FloatLayout): pass
         
-
 class RootWidget(ScreenManager): pass
 
 class MainApp(App):
